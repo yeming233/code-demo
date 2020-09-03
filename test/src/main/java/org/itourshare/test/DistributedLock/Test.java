1388082b4429efedaf32fd9e3efd354d6348ee70
yeming <yeming233@126.com>
2020-09-03 10:55:09 +0800

    add test

---
+++ test/src/main/java/org/itourshare/test/DistributedLock/Test.java
@@ -0,0 +1,26 @@
+package org.itourshare.test.DistributedLock;
+
+
+
+public class Test {
+    public static void main(String[] args) {
+        Service service = new Service();
+        for (int i = 0; i < 50; i++) {
+            ThreadA threadA = new ThreadA(service);
+            threadA.start();
+        }
+    }
+}
+
+class ThreadA extends Thread {
+    private Service service;
+
+    public ThreadA(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.seckill();
+    }
+}
---
+++ test/src/main/java/org/itourshare/test/Test.java
@@ -0,0 +1,12 @@
+package org.itourshare.test;
+
+/**
+ * @ClassName : Test
+ * @Description :
+ * @Author : its
+ * @Date: 2020-09-03 08:58
+ */
+public class Test {
+    public static void main(String[] args) {
+    }
+}
