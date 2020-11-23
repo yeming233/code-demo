eb987156b7418845d3eb9c16e90f90a81763a592
yy <yeming233@126.com>
2020-11-23 10:30:53 +0800

    add test

--- test/src/main/java/org/itourshare/test/Test.java
+++ test/src/main/java/org/itourshare/test/Test.java
@@ -1,5 +1,11 @@
 package org.itourshare.test;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 /**
  * @ClassName : Test
  * @Description :
@@ -7,6 +13,12 @@ package org.itourshare.test;
  * @Date: 2020-09-03 08:58
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        LocalDateTime now = LocalDateTime.now();
+        Thread.sleep(1999);
+        LocalDateTime expire = LocalDateTime.now();
+        long delay = ChronoUnit.MILLIS.between(now, expire);
+        System.out.println(delay);
     }
 }
